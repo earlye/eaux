63b208ac09ffec85e4fcb8a8b3ef84fcda767160
Early Ehlinger [cerby] <earlye@gmail.com>
2026-03-02 09:07:00 -0600

    Improve docs

--- go/paths/UserDirectory/ExpandUserDirectory.go
+++ go/paths/UserDirectory/ExpandUserDirectory.go
@@ -3,6 +3,8 @@ package userdirectory
 // coverage-ignore-file
 
 // ExpandUserDirectory expands a path possibly prefixed with `~` to the full path.
+//
+// Equivalent to: `ExpandUserDirectoryEx(path, GetUserDirectory)`
 func ExpandUserDirectory(path string) (result string, err error) {
 	return ExpandUserDirectoryEx(path, GetUserDirectory)
 }
--- go/paths/UserDirectory/GetUserDirectory.go
+++ go/paths/UserDirectory/GetUserDirectory.go
@@ -10,6 +10,8 @@ import (
 // GetUserDirectory gets the user directory for a given name.
 // An empty name for the user will return the home directory of the current user.
 // If the directory cannot be resolved, an error is returned.
+//
+// Equivalent to: `GetUserDirectoryEx(name, os.UserHomeDir, user.Lookup)`
 func GetUserDirectory(name string) (result string, err error) {
 	return GetUserDirectoryEx(name, os.UserHomeDir, user.Lookup)
 }
