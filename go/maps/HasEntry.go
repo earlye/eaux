39e8951c8046c599f09ee134401083da987012f0
Early Ehlinger [cerby] <earlye@gmail.com>
2026-03-10 16:14:48 -0500

    fix maps.HasEntry

--- go/maps/HasEntry.go
+++ go/maps/HasEntry.go
@@ -1,7 +1,7 @@
 package maps
 
 // HasEntry returns true if the key is present in m.
-func HasEntry[K comparable, V any](m map[K]V, key K, defaultValue V) bool {
+func HasEntry[K comparable, V any](m map[K]V, key K) bool {
 	_, ok := m[key]
 	return ok
 }
